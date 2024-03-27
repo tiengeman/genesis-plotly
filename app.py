b07780ba4a8b85ff35a0e60cb8e32ef82358aa98
EDUARDO DIAS DE OLIVEIRA TELES <eduardo.teles@engemancloud.intra>
2024-03-27 11:50:09 -0300

    começo do calculo reativo

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from banco import *
-from shiny import ui, render, App, reactive
-from shiny.express import ui
+from shiny import render, reactive
+from shiny.express import input, ui
 
 ui.h2("Simple Interactive Table")
-with ui.sidebar(open="closed", bg="#4972FF"):
+with ui.sidebar(open="closed", bg="#FF4500"):
         ui.input_selectize(
-        "var", "Select variable",
-        ["1", "2", "3", "4", "5"]
+        "var", "Selecione a competência",
+        ["01/2024", "02/2024", "03/2024", "04/2024", "05/2024"]
     )
 
-df = tabela()
+@render.text
+def comp():
+      return f'competência: {input.var()}'
+
+# @reactive.cal
+# def df():
+#       return tabela(input.var())
+
 @render.data_frame  
 def data_df():
+    #passa o valor do input na função tabela input.var()
+    df = tabela()
     return render.DataGrid(df, summary=False, row_selection_mode="single")
