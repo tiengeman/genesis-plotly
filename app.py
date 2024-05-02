c1c447c534b67d1ce1321c94738a5340d64c39ff
EDUARDO DIAS DE OLIVEIRA TELES <eduardo.teles@engemancloud.intra>
2024-05-02 13:08:53 -0300

    tabela capex

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ app.layout = html.Div(style={'fontFamily': 'Arial, sans-serif'}, children=[
         style={'fontFamily': 'Arial, sans-serif'}  # Definindo a fonte da lista suspensa
     ),
     html.Div(style={'margin-top': '20px'}),  # Espaçamento entre dropdown e tabela
-    html.Div(id='tabela-container', style={'margin': '20px'})  # Div para mostrar a tabela selecionada com margem
+    html.Div(id='tabela-container', style={'margin': '20px'}),  # Div para mostrar a tabela selecionada com margem
+    html.Div(id='tabela2-container', style={'margin': '20px'})  # Div para mostrar a tabela selecionada com margem
 ])
 
 # Callback para atualizar a tabela com base na seleção da lista suspensa
@@ -69,7 +70,125 @@ def atualizar_tabela(selecao):
                 {
                     'if': {'column_id': c},
                     'textAlign': 'right'
-                } for c in ['(R) MEDIÇÃO', 'MEDIÇÃO TOTAL']
+                } for c in ['(R) MEDIÇÃO', '(D) DESPESAS', '(R-D) LUCRO', 'MEDIÇÃO TOTAL', 'DESPESAS TOTAIS', 'LUCRO TOTAL']
+            ],
+            style_header={
+                'fontWeight': 'bold',
+                'backgroundColor': 'rgb(220,220,220)',
+            },
+            style_data={
+                'color': 'black',
+                'backgroundColor': 'white',
+            },
+            style_data_conditional=[
+                {
+                    'if': {'row_index': 'odd'},
+                    'backgroundColor': 'rgb(245,245,245)',
+                },
+                {
+                    'if': {
+                        'filter_query': "{CONTRATO} = 'TOTAL OPERAÇÃO'",
+                    },
+                    'backgroundColor': 'rgb(220,220,220)',
+                    'fontWeight': 'bold',
+                },
+                {
+                    'if': {
+                        'filter_query': "{%} < 0",
+                        'column_id': '%',
+                    },
+                    'backgroundColor': '#B22222',
+                    'color': 'white',
+                },
+                {
+                    'if': {
+                        'filter_query': "{%} > 0.07",
+                        'column_id': '%',
+                    },
+                    'backgroundColor': '#008000',
+                    'color': 'white',
+                },
+                {
+                    'if': {
+                        'filter_query': "{%} > 0 && {%} < 0.07",
+                        'column_id': '%',
+                    },
+                    'backgroundColor': '#DAA520',
+                    'color': 'white',
+                },
+                {
+                    'if': {
+                        'filter_query': "{% TOTAL} < 0",
+                        'column_id': '% TOTAL',
+                    },
+                    'backgroundColor': '#B22222',
+                    'color': 'white',
+                },
+                {
+                    'if': {
+                        'filter_query': "{% TOTAL} > 0.07",
+                        'column_id': '% TOTAL',
+                    },
+                    'backgroundColor': '#008000',
+                    'color': 'white',   
+                },
+                {
+                    'if': {
+                        'filter_query': "{% TOTAL} > 0 && {% TOTAL} < 0.07",
+                        'column_id': '% TOTAL',
+                    },
+                    'backgroundColor': '#DAA520',
+                    'color': 'white',
+                }
+            ], 
+        )
+        
+        return tabela_datatable
+    else:
+        # Se não houver seleção, retorna uma mensagem indicando que nenhuma tabela está disponível
+        return html.P("Nenhuma tabela disponível para esta seleção.")
+    
+# Callback para atualizar a tabela com base na seleção da lista suspensa
+@app.callback(
+    Output('tabela2-container', 'children'),
+    [Input('minha-lista-suspensa', 'value')]
+)
+def atualizar_tabela2(selecao):
+    # Verifica se há uma seleção
+    if selecao:
+        # Obtém a tabela com base na seleção da lista suspensa
+        df_tabela = tabela_2(selecao)
+        
+        # Formatar colunas numéricas para o estilo brasileiro
+        for coluna in ['(R) MEDIÇÃO', '(D) DESPESAS', '(R-D) LUCRO', 'MEDIÇÃO TOTAL', 'DESPESAS TOTAIS', 'LUCRO TOTAL']:
+            df_tabela[coluna] = df_tabela[coluna].apply(lambda x: locale.format_string('%1.2f', x, grouping=True))
+        
+        # Cria DataTable a partir do DataFrame
+        tabela_datatable = dash_table.DataTable(
+            id='tabela',
+            data=df_tabela.to_dict('records'),
+            filter_action="native",
+            columns=[
+            {'name': 'LOCAL', 'id': 'LOCAL', 'type': 'text'},
+            {'name': 'CONTRATO', 'id': 'CONTRATO', 'type': 'text'},
+            {'name': 'C.CUSTOS', 'id': 'C.CUSTOS', 'type': 'text'},
+            {'name': 'INATIVO', 'id': 'INATIVO', 'type': 'text'},
+            {'name': 'FILIAL', 'id': 'FILIAL', 'type': 'text'},
+            {'name': '(R) MEDIÇÃO', 'id': '(R) MEDIÇÃO', 'type': 'numeric'},
+            {'name': '(D) DESPESAS', 'id': '(D) DESPESAS', 'type': 'numeric'},
+            {'name': '(R-D) LUCRO', 'id': '(R-D) LUCRO', 'type': 'numeric'},
+            {'name': '%', 'id': '%', 'type': 'numeric', 'format': FormatTemplate.percentage(1)},
+            {'name': 'MEDIÇÃO TOTAL', 'id': 'MEDIÇÃO TOTAL', 'type': 'numeric'},
+            {'name': 'DESPESAS TOTAIS', 'id': 'DESPESAS TOTAIS', 'type': 'numeric'},
+            {'name': 'LUCRO TOTAL', 'id': 'LUCRO TOTAL', 'type': 'numeric'},
+            {'name': '% TOTAL', 'id': '% TOTAL', 'type': 'numeric', 'format': FormatTemplate.percentage(1)},
+        ],
+            style_cell={'textAlign': 'center', 'padding': '5px', 'fontFamily': 'Arial, sans-serif', 'fontSize': '0.8em'},  # Ajustando o tamanho da fonte
+            style_cell_conditional=[
+                {
+                    'if': {'column_id': c},
+                    'textAlign': 'right'
+                } for c in ['(R) MEDIÇÃO', '(D) DESPESAS', '(R-D) LUCRO', 'MEDIÇÃO TOTAL', 'DESPESAS TOTAIS', 'LUCRO TOTAL']
             ],
             style_header={
                 'fontWeight': 'bold',
--- banco.py
+++ banco.py
@@ -50,6 +50,38 @@ def tabela(mes): #função que gera a tabela principal do resumo
     
     return df
 
+def tabela_2(mes):
+    lista_contrato = ['INVESTIMENTOS (CONTRATO)', 'EXPANSÃO - FILIAL MACAÉ', 'EXPANSÃO - MATRIZ RECIFE', 'DEPÓSITOS JUDICIAIS', 'ENGEMAN TECNOLOGIAS']
+    list_local = ['CAPEX']*len(lista_contrato)
+    lista_cc = ['888', '2180', '2250', '1111', '1930']
+    list_inativo = [None]*len(lista_contrato)
+    list_filial = [None]*len(lista_contrato)
+    lista_soma_comp = medicao_capex(mes)
+    tupla_despesas = back.total_despesa_competencia(mes) #gera uma lista de tuplas com as info
+    list_despesas = ordena_lista(lista_contrato, tupla_despesas) #aqui ele ordena os valores de acordo com a lista de contrato
+    list_lucro = subtrair_listas(lista_soma_comp, list_despesas)
+    list_perc = list_perc = perc(lista_soma_comp, list_lucro)
+    list_medicao_total = medicao_capex_total()
+    tupla_despesa_total = back.total_despesa()
+    list_desp_totais = ordena_lista(lista_contrato, tupla_despesa_total)
+    list_lucro_total = subtrair_listas(list_medicao_total, list_desp_totais)
+    list_perc_total = perc(list_medicao_total, list_lucro_total)
+    df = pd.DataFrame.from_dict(data={'LOCAL':list_local, 
+                                      'CONTRATO':lista_contrato, 
+                                      'C.CUSTOS':lista_cc, 
+                                      'INATIVO':list_inativo,
+                                      'FILIAL':list_filial, 
+                                      '(R) MEDIÇÃO':lista_soma_comp, 
+                                      '(D) DESPESAS':list_despesas, 
+                                      '(R-D) LUCRO':list_lucro, 
+                                      '%':list_perc,
+                                      'MEDIÇÃO TOTAL': list_medicao_total,
+                                      'DESPESAS TOTAIS': list_desp_totais,
+                                      'LUCRO TOTAL': list_lucro_total,
+                                      '% TOTAL': list_perc_total})
+    
+    return df
+
 def inativo(lista_valores): #função para criar a lista se o contrato está inativo ou não
     lista_inativo = []
     for i in lista_valores:
@@ -101,3 +133,33 @@ def merge_lists_into_tuples(list1, list2): #função para juntar duas listas e f
     # Usar zip para combinar as listas em uma lista de tuplas
     merged_list = list(zip(list1, list2))
     return merged_list
+
+def medicao_capex(mes):
+    lista_capex = ['INVESTIMENTOS (CONTRATO)', 'EXPANSÃO - FILIAL MACAÉ', 'EXPANSÃO - MATRIZ RECIFE', 'DEPÓSITOS JUDICIAIS', 'ENGEMAN TECNOLOGIAS']
+    medicao = []
+    lista_contrato, lista_soma_comp, lista_cc = back.medicao(mes)
+    for i in lista_capex:
+        flag = False
+        for a in range(len(lista_contrato)):
+            if i == lista_contrato[a]:
+                medicao.append(lista_soma_comp[a])
+                flag = True
+        if flag == False:
+            medicao.append(0)
+
+    return medicao
+        
+def medicao_capex_total():
+    lista_capex = ['INVESTIMENTOS (CONTRATO)', 'EXPANSÃO - FILIAL MACAÉ', 'EXPANSÃO - MATRIZ RECIFE', 'DEPÓSITOS JUDICIAIS', 'ENGEMAN TECNOLOGIAS']
+    medicao_total = []
+    lista_cont_total, lista_valor_total, lista_cc_total = back.medicao_total()
+    for i in lista_capex:
+        flag = False
+        for a in range(len(lista_cont_total)):
+            if i == lista_cont_total[a]:
+                medicao_total.append(lista_valor_total[a])
+                flag = True
+        if flag == False:
+            medicao_total.append(0)
+
+    return medicao_total
