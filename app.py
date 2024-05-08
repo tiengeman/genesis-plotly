28a73e9e4477b5f24494a7ac7634f1e66d4bd0eb
EDUARDO DIAS DE OLIVEIRA TELES <eduardo.teles@engemancloud.intra>
2024-05-08 14:45:37 -0300

    adicionado menu

--- app.py
+++ app.py
@@ -1,164 +1,68 @@
+# app.py
 import dash
-from banco import *
-from dash import dcc, html, dash_table
+import dash_bootstrap_components as dbc
+from dash import html, dcc
 from dash.dependencies import Input, Output
-import back
-import locale
-from dash.dash_table.Format import Format, Scheme, Sign, Symbol
-import dash.dash_table.FormatTemplate as FormatTemplate
+import pages.pagina1 as pagina1
 
-app = dash.Dash(__name__)
-server = app.server
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# Paleta de cores
-colors = {
-    'background': '#EFEEEE',  # Cinza claro para o fundo da página
-    'text': '#333333',        # Cor de texto principal em preto
-    'orange': '#FF4E00',      # Laranja
-    'white': '#FFFFFF',       # Branco
-    'gray': '#CCCCCC'         # Cinza claro para elementos secundários
-}
+# Layout
+sidebar = dbc.Nav(
+    [
+        dbc.NavItem(dbc.NavLink('Home', href='/home', className='nav-link')),
+        dbc.NavItem(dbc.NavLink('Page 1', href='/page1', className='nav-link')),
+        #...
+    ],
+    vertical=True,  # Make the nav items stack vertically
+    pills=True,  # Make the nav items take up the full width of the sidebar
+    className='bg-light'  # Add a light background color to the sidebar
+)
 
-# Layout do aplicativo
-app.layout = html.Div(style={'fontFamily': 'Arial, sans-serif'}, children=[
-    html.H2(children='Performance Mensal por Competência', style={'color': colors['orange'], 'textAlign': 'center', 'arginTop': '20px', 'arginBottom': '20px', 'fontSize': '2em'}),
-    html.Hr(style={'backgroundColor': colors['orange']}),  # Linha horizontal laranja
-    html.Div(style={'marginTop': '20px'}),
-    # Lista suspensa
-    dcc.Dropdown(
-        id='minha-lista-suspensa',
-        options=back.competencias(),
-        value=back.competencias()[-1],
-        style={'fontFamily': 'Arial, sans-serif'}  # Definindo a fonte da lista suspensa
-    ),
-    html.Div(style={'marginTop': '20px'}),  # Espaçamento entre dropdown e tabela
-    html.Div(id='tabela-container', style={'margin': '20px'}),  # Div para mostrar a tabela selecionada com margem
-    html.Div(id='tabela2-container', style={'margin': '20px'})  # Div para mostrar a tabela selecionada com margem
+content_area = html.Div(id='content')
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    dbc.Row([
+        dbc.Col(sidebar, width=1, className='bg-light'),  # Make the sidebar take up 2 columns
+        dbc.Col(content_area, width=10)  # Make the content area take up 10 columns
+    ])
 ])
 
-# Callback para atualizar a tabela com base na seleção da lista suspensa
+# Callbacks
+@app.callback(
+    Output('content', 'children'),
+    [Input('url', 'pathname')]
+)
+def update_content(pathname):
+    if pathname == '/page1':
+        return pagina1.layout
+    else:
+        return html.Div([
+            html.H1('Home'),
+            html.P('This is a simple text message.')
+        ])
+
+def update_table(output_id, value):
+    if output_id == 'tabela-container':
+        return pagina1.atualizar_tabela(value)
+    elif output_id == 'tabela2-container':
+        return pagina1.atualizar_tabela2(value)
+
+
 @app.callback(
     Output('tabela-container', 'children'),
     [Input('minha-lista-suspensa', 'value')]
 )
-def atualizar_tabela(selecao):
-    if selecao:
-        df_tabela = tabela(selecao)
-        format_numeric_columns(df_tabela, ['(R) MEDIÇÃO', '(D) DESPESAS', '(R-D) LUCRO', 'MEDIÇÃO TOTAL', 'DESPESAS TOTAIS', 'LUCRO TOTAL'])
-        return create_datatable(df_tabela, colors)
-    else:
-        return html.P("Nenhuma tabela disponível para esta seleção.")
+def update_table1(value):
+    return update_table('tabela-container', value)
 
 @app.callback(
     Output('tabela2-container', 'children'),
     [Input('minha-lista-suspensa', 'value')]
 )
-def atualizar_tabela2(selecao):
-    if selecao:
-        df_tabela = tabela_2(selecao)
-        format_numeric_columns(df_tabela, ['(R) MEDIÇÃO', '(D) DESPESAS', '(R-D) LUCRO', 'MEDIÇÃO TOTAL', 'DESPESAS TOTAIS', 'LUCRO TOTAL'])
-        return create_datatable(df_tabela, colors)
-    else:
-        return html.P("Nenhuma tabela disponível para esta seleção.")
-
-def format_numeric_columns(df, columns):
-    for col in columns:
-        df[col] = df[col].apply(lambda x: locale.format_string('%1.2f', x, grouping=True))
-
-def create_datatable(df, colors):
-    return dash_table.DataTable(
-        id='tabela',
-        data=df.to_dict('records'),
-        filter_action="native",
-        columns=[
-            {'name': 'LOCAL', 'id': 'LOCAL', 'type': 'text'},
-            {'name': 'CONTRATO', 'id': 'CONTRATO', 'type': 'text'},
-            {'name': 'C.CUSTOS', 'id': 'C.CUSTOS', 'type': 'text'},
-            {'name': 'INATIVO', 'id': 'INATIVO', 'type': 'text'},
-            {'name': 'FILIAL', 'id': 'FILIAL', 'type': 'text'},
-            {'name': '(R) MEDIÇÃO', 'id': '(R) MEDIÇÃO', 'type': 'numeric'},
-            {'name': '(D) DESPESAS', 'id': '(D) DESPESAS', 'type': 'numeric'},
-            {'name': '(R-D) LUCRO', 'id': '(R-D) LUCRO', 'type': 'numeric'},
-            {'name': '%', 'id': '%', 'type': 'numeric', 'format': FormatTemplate.percentage(1)},
-            {'name': 'MEDIÇÃO TOTAL', 'id': 'MEDIÇÃO TOTAL', 'type': 'numeric'},
-            {'name': 'DESPESAS TOTAIS', 'id': 'DESPESAS TOTAIS', 'type': 'numeric'},
-            {'name': 'LUCRO TOTAL', 'id': 'LUCRO TOTAL', 'type': 'numeric'},
-            {'name': '% TOTAL', 'id': '% TOTAL', 'type': 'numeric', 'format': FormatTemplate.percentage(1)},
-        ],
-        style_cell={'textAlign': 'center', 'padding': '5px', 'fontFamily': 'Arial, sans-serif', 'fontSize': '0.8em', 'backgroundColor': colors['white'], 'color': colors['text']},  # Ajustando o tamanho da fonte
-        style_cell_conditional=[
-            {
-                'if': {'column_id': c},
-                'textAlign': 'right'
-            } for c in ['(R) MEDIÇÃO', '(D) DESPESAS', '(R-D) LUCRO', 'MEDIÇÃO TOTAL', 'DESPESAS TOTAIS', 'LUCRO TOTAL']
-        ],
-        style_header={
-            'fontWeight': 'bold',
-            'backgroundColor': colors['orange'],
-            'color': colors['white'],
-        },
-        style_data_conditional=[
-            {
-                'if': {'row_index': 'odd'},
-                'backgroundColor': colors['background'],
-            },
-            {
-                'if': {
-                    'filter_query': "{CONTRATO} = 'TOTAL OPERAÇÃO'",
-                },
-                'backgroundColor': colors['gray'],
-                'fontWeight': 'bold',
-            },
-            {
-                'if': {
-                    'filter_query': "{%} < 0",
-                    'column_id': '%',
-                },
-                'backgroundColor': '#B22222',
-                'color': 'white',
-            },
-            {
-                'if': {
-                    'filter_query': "{%} > 0.07",
-                    'column_id': '%',
-                },
-                'backgroundColor': '#008000',
-                'color': 'white',
-            },
-            {
-                'if': {
-                    'filter_query': "{%} > 0 && {%} < 0.07",
-                    'column_id': '%',
-                },
-                'backgroundColor': '#DAA520',
-                'color': 'white',
-            },
-            {
-                'if': {
-                    'filter_query': "{% TOTAL} < 0",
-                    'column_id': '% TOTAL',
-                },
-                'backgroundColor': '#B22222',
-                'color': 'white',
-            },
-            {
-                'if': {
-                    'filter_query': "{% TOTAL} > 0.07",
-                    'column_id': '% TOTAL',
-                },
-                'backgroundColor': '#008000',
-                'color': 'white',   
-            },
-            {
-                'if': {
-                    'filter_query': "{% TOTAL} > 0 && {% TOTAL} < 0.07",
-                    'column_id': '% TOTAL',
-                },
-                'backgroundColor': '#DAA520',
-                'color': 'white',
-            }
-        ], 
-    )
+def update_table2(value):
+    return update_table('tabela2-container', value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ pages/pagina1.py
@@ -0,0 +1,148 @@
+from dash import html, dcc
+import dash
+from dash.dependencies import Input, Output
+import dash_table
+import locale
+from dash.dash_table.Format import Format, Scheme, Sign, Symbol
+import dash.dash_table.FormatTemplate as FormatTemplate
+from banco import *
+
+# Paleta de cores
+colors = {
+    'background': '#EFEEEE',  # Cinza claro para o fundo da página
+    'text': '#333333',        # Cor de texto principal em preto
+    'orange': '#FF4E00',      # Laranja
+    'white': '#FFFFFF',       # Branco
+    'gray': '#CCCCCC'         # Cinza claro para elementos secundários
+}
+
+layout = html.Div(style={'fontFamily': 'Arial, sans-serif'}, children=[
+    html.H2(children='Performance Mensal por Competência', style={'color': colors['orange'], 'textAlign': 'center', 'arginTop': '20px', 'arginBottom': '20px', 'fontSize': '2em'}),
+    html.Hr(style={'backgroundColor': colors['orange']}),  # Linha horizontal laranja
+    html.Div(style={'marginTop': '20px'}),
+    # Lista suspensa
+    dcc.Dropdown(
+        id='minha-lista-suspensa',
+        options=back.competencias(),
+        value=back.competencias()[-1],
+        style={'fontFamily': 'Arial, sans-serif'}  # Definindo a fonte da lista suspensa
+    ),
+    html.Div(style={'marginTop': '20px'}),  # Espaçamento entre dropdown e tabela
+    html.Div(id='tabela-container'),  # Div para mostrar a tabela selecionada com margem
+    html.Div(id='tabela2-container')  # Div para mostrar a tabela selecionada com margem
+])
+
+def atualizar_tabela(selecao):
+    if selecao:
+        df_tabela = tabela(selecao)
+        format_numeric_columns(df_tabela, ['(R) MEDIÇÃO', '(D) DESPESAS', '(R-D) LUCRO', 'MEDIÇÃO TOTAL', 'DESPESAS TOTAIS', 'LUCRO TOTAL'])
+        return create_datatable(df_tabela, colors)
+    else:
+        return html.P("Nenhuma tabela disponível para esta seleção.")
+
+def atualizar_tabela2(selecao):
+    if selecao:
+        df_tabela = tabela_2(selecao)
+        format_numeric_columns(df_tabela, ['(R) MEDIÇÃO', '(D) DESPESAS', '(R-D) LUCRO', 'MEDIÇÃO TOTAL', 'DESPESAS TOTAIS', 'LUCRO TOTAL'])
+        return create_datatable(df_tabela, colors)
+    else:
+        return html.P("Nenhuma tabela disponível para esta seleção.")
+
+def format_numeric_columns(df, columns):
+    for col in columns:
+        df[col] = df[col].apply(lambda x: locale.format_string('%1.2f', x, grouping=True))
+
+def create_datatable(df, colors):
+    return dash_table.DataTable(
+        id='tabela',
+        data=df.to_dict('records'),
+        filter_action="native",
+        columns=[
+            {'name': 'LOCAL', 'id': 'LOCAL', 'type': 'text'},
+            {'name': 'CONTRATO', 'id': 'CONTRATO', 'type': 'text'},
+            {'name': 'C.CUSTOS', 'id': 'C.CUSTOS', 'type': 'text'},
+            {'name': 'INATIVO', 'id': 'INATIVO', 'type': 'text'},
+            {'name': 'FILIAL', 'id': 'FILIAL', 'type': 'text'},
+            {'name': '(R) MEDIÇÃO', 'id': '(R) MEDIÇÃO', 'type': 'numeric'},
+            {'name': '(D) DESPESAS', 'id': '(D) DESPESAS', 'type': 'numeric'},
+            {'name': '(R-D) LUCRO', 'id': '(R-D) LUCRO', 'type': 'numeric'},
+            {'name': '%', 'id': '%', 'type': 'numeric', 'format': FormatTemplate.percentage(1)},
+            {'name': 'MEDIÇÃO TOTAL', 'id': 'MEDIÇÃO TOTAL', 'type': 'numeric'},
+            {'name': 'DESPESAS TOTAIS', 'id': 'DESPESAS TOTAIS', 'type': 'numeric'},
+            {'name': 'LUCRO TOTAL', 'id': 'LUCRO TOTAL', 'type': 'numeric'},
+            {'name': '% TOTAL', 'id': '% TOTAL', 'type': 'numeric', 'format': FormatTemplate.percentage(1)},
+        ],
+        style_cell={'textAlign': 'center', 'padding': '5px', 'fontFamily': 'Arial, sans-serif', 'fontSize': '0.8em', 'backgroundColor': colors['white'], 'color': colors['text']},  # Ajustando o tamanho da fonte
+        style_cell_conditional=[
+            {
+                'if': {'column_id': c},
+                'textAlign': 'right'
+            } for c in ['(R) MEDIÇÃO', '(D) DESPESAS', '(R-D) LUCRO', 'MEDIÇÃO TOTAL', 'DESPESAS TOTAIS', 'LUCRO TOTAL']
+        ],
+        style_header={
+            'fontWeight': 'bold',
+            'backgroundColor': colors['orange'],
+            'color': colors['white'],
+        },
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': colors['background'],
+            },
+            {
+                'if': {
+                    'filter_query': "{CONTRATO} = 'TOTAL OPERAÇÃO'",
+                },
+                'backgroundColor': colors['gray'],
+                'fontWeight': 'bold',
+            },
+            {
+                'if': {
+                    'filter_query': "{%} < 0",
+                    'column_id': '%',
+                },
+                'backgroundColor': '#B22222',
+                'color': 'white',
+            },
+            {
+                'if': {
+                    'filter_query': "{%} > 0.07",
+                    'column_id': '%',
+                },
+                'backgroundColor': '#008000',
+                'color': 'white',
+            },
+            {
+                'if': {
+                    'filter_query': "{%} > 0 && {%} < 0.07",
+                    'column_id': '%',
+                },
+                'backgroundColor': '#DAA520',
+                'color': 'white',
+            },
+            {
+                'if': {
+                    'filter_query': "{% TOTAL} < 0",
+                    'column_id': '% TOTAL',
+                },
+                'backgroundColor': '#B22222',
+                'color': 'white',
+            },
+            {
+                'if': {
+                    'filter_query': "{% TOTAL} > 0.07",
+                    'column_id': '% TOTAL',
+                },
+                'backgroundColor': '#008000',
+                'color': 'white',   
+            },
+            {
+                'if': {
+                    'filter_query': "{% TOTAL} > 0 && {% TOTAL} < 0.07",
+                    'column_id': '% TOTAL',
+                },
+                'backgroundColor': '#DAA520',
+                'color': 'white',
+            }
+        ], 
+    )
