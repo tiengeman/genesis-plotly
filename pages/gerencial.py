d67e06ab648fdb02a226c7265ef189f8a62b0ccc
EDUARDO DIAS DE OLIVEIRA TELES <eduardo.teles@engemancloud.intra>
2024-05-13 13:03:28 -0300

    column hide

--- pages/gerencial.py
+++ pages/gerencial.py
@@ -1,9 +1,6 @@
 from dash import html, dcc
-import dash
-from dash.dependencies import Input, Output
-import dash_table
+from dash import dash_table
 import locale
-from dash.dash_table.Format import Format, Scheme, Sign, Symbol
 import dash.dash_table.FormatTemplate as FormatTemplate
 from banco import *
 
@@ -18,7 +15,7 @@ colors = {
 
 # Define a layout with a centered container
 layout = html.Div(style={'fontFamily': 'Arial, sans-serif', 'textAlign': 'center'}, children=[
-    html.H1(children='Performance Mensal por Competência', style={'color': colors['orange'], 'fontSize': '2em', 'marginTop': '10px'}),
+    html.H1(children='Performance Mensal por Competência', style={'color': colors['orange'], 'marginTop': '10px'}),
     html.Hr(style={'backgroundColor': colors['orange']}),  # Linha horizontal laranja
     html.Div(style={'marginTop': '20px'}),
     # Lista suspensa
@@ -60,30 +57,39 @@ def create_datatable(df, colors):
         data=df.to_dict('records'),
         filter_action="native",
         columns=[
-            {'name': 'LOCAL', 'id': 'LOCAL', 'type': 'text'},
+            {'name': 'LOCAL', 'id': 'LOCAL', 'type': 'text', "hideable": True},
             {'name': 'CONTRATO', 'id': 'CONTRATO', 'type': 'text'},
-            {'name': 'C.CUSTOS', 'id': 'C.CUSTOS', 'type': 'text'},
-            {'name': 'INATIVO', 'id': 'INATIVO', 'type': 'text'},
-            {'name': 'FILIAL', 'id': 'FILIAL', 'type': 'text'},
-            {'name': '(R) MEDIÇÃO', 'id': '(R) MEDIÇÃO', 'type': 'numeric'},
-            {'name': '(D) DESPESAS', 'id': '(D) DESPESAS', 'type': 'numeric'},
-            {'name': '(R-D) LUCRO', 'id': '(R-D) LUCRO', 'type': 'numeric'},
-            {'name': '%', 'id': '%', 'type': 'numeric', 'format': FormatTemplate.percentage(1)},
-            {'name': 'MEDIÇÃO TOTAL', 'id': 'MEDIÇÃO TOTAL', 'type': 'numeric'},
-            {'name': 'DESPESAS TOTAIS', 'id': 'DESPESAS TOTAIS', 'type': 'numeric'},
-            {'name': 'LUCRO TOTAL', 'id': 'LUCRO TOTAL', 'type': 'numeric'},
-            {'name': '% TOTAL', 'id': '% TOTAL', 'type': 'numeric', 'format': FormatTemplate.percentage(1)},
+            {'name': 'CC', 'id': 'C.CUSTOS', 'type': 'text', "hideable": True},
+            {'name': 'INATIVO', 'id': 'INATIVO', 'type': 'text', "hideable": True},
+            {'name': 'FILIAL', 'id': 'FILIAL', 'type': 'text', "hideable": True},
+            {'name': 'MEDIÇÃO', 'id': '(R) MEDIÇÃO', 'type': 'numeric', "hideable": True},
+            {'name': 'DESPESAS', 'id': '(D) DESPESAS', 'type': 'numeric', "hideable": True},
+            {'name': 'LUCRO', 'id': '(R-D) LUCRO', 'type': 'numeric', "hideable": True},
+            {'name': '%', 'id': '%', 'type': 'numeric', 'format': FormatTemplate.percentage(1), "hideable": True},
+            {'name': 'MEDIÇÃO TOTAL', 'id': 'MEDIÇÃO TOTAL', 'type': 'numeric', "hideable": True},
+            {'name': 'DESP. TOTAIS', 'id': 'DESPESAS TOTAIS', 'type': 'numeric', "hideable": True},
+            {'name': 'LUCRO TOTAL', 'id': 'LUCRO TOTAL', 'type': 'numeric', "hideable": True},
+            {'name': '%', 'id': '% TOTAL', 'type': 'numeric', 'format': FormatTemplate.percentage(1), "hideable": True},
         ],
         style_cell={'textAlign': 'center', 'padding': '5px', 'fontFamily': 'Arial, sans-serif', 'fontSize': '0.8em', 'backgroundColor': colors['white'], 'color': colors['text']},  # Ajustando o tamanho da fonte
-        style_cell_conditional=[
-            {
-                'if': {'column_id': c},
-                'textAlign': 'right'
-            } for c in ['(R) MEDIÇÃO', '(D) DESPESAS', '(R-D) LUCRO', 'MEDIÇÃO TOTAL', 'DESPESAS TOTAIS', 'LUCRO TOTAL']
+        style_cell_conditional = [
+            {'if': {'column_id': 'LOCAL'}, 'width': '4%'},
+            {'if': {'column_id': 'CONTRATO'}, 'width': '35%'},
+            {'if': {'column_id': 'C.CUSTOS'}, 'width': '4%'},
+            {'if': {'column_id': 'INATIVO'}, 'width': '4%'},
+            {'if': {'column_id': 'FILIAL'}, 'width': '4%'},
+            {'if': {'column_id': '(R) MEDIÇÃO'}, 'width': '6%', 'textAlign': 'right'},
+            {'if': {'column_id': '(D) DESPESAS'}, 'width': '7%', 'textAlign': 'right'},
+            {'if': {'column_id': '(R-D) LUCRO'}, 'width': '7%', 'textAlign': 'right'},
+            {'if': {'column_id': '%'}, 'width': '4%', 'textAlign': 'right'},
+            {'if': {'column_id': 'MEDIÇÃO TOTAL'}, 'width': '7%', 'textAlign': 'right'},
+            {'if': {'column_id': 'DESPESAS TOTAIS'}, 'width': '7%', 'textAlign': 'right'},
+            {'if': {'column_id': 'LUCRO TOTAL'}, 'width': '7%', 'textAlign': 'right'},
+            {'if': {'column_id': '% TOTAL'}, 'width': '4%', 'textAlign': 'right'},
         ],
         style_header={
             'fontWeight': 'bold',
-            'backgroundColor': colors['orange'],
+            'backgroundColor': '#616468',
             'color': colors['white'],
         },
         style_data_conditional=[
@@ -112,7 +118,7 @@ def create_datatable(df, colors):
                     'column_id': '%',
                 },
                 'backgroundColor': '#008000',
-                'color': 'white',
+                'color': 'white',   
             },
             {
                 'if': {
