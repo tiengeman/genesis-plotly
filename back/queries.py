2ee629eb5f0557eb90d5f251277af61c6c0820ad
IanFelipe215 <ianfelipinhonet@gmail.com>
2024-06-13 09:49:55 -0300

    alterando função detalha_despesas

--- back/queries.py
+++ back/queries.py
@@ -78,19 +78,19 @@ def total_despesa(db=back.db):
 
     # filtros que agrupam os campos descricao-projeto e somam os valores de valor-original-despesa de cada contrato
     pipeline_financeiro =  [ 
-        {'$group':{'_id':{'codigo-projeto-unificado':'$codigo-projeto-unificado'},'despesa_financeiro':{'$sum':'$valor-despesa'}}}
+        {'$group':{'_id':{'codigo-projeto-unificado':'$codigo-projeto-unificado'},'despesa_financeiro':{'$sum':'$valor-despesa','depesa_ivestimento':{'$sum':'$valor-investimento-despesa'}}}}
     ]
     pipeline_folha  = [
-        {'$group':{'_id':{'codigo-projeto-unificado':'$codigo-projeto-unificado'},'despesa_folha':{'$sum':'$valor-despesa'}}}
+        {'$group':{'_id':{'codigo-projeto-unificado':'$codigo-projeto-unificado'},'despesa_folha':{'$sum':'$valor-despesa','depesa_ivestimento':{'$sum':'$valor-investimento-despesa'}}}}
     ]
     pipeline_relatorio = [
-        {'$group':{'_id':{'codigo-projeto-unificado':'$codigo-projeto-unificado'},'despesa_relatorio':{'$sum':'$valor-despesa'}}}
+        {'$group':{'_id':{'codigo-projeto-unificado':'$codigo-projeto-unificado'},'despesa_relatorio':{'$sum':'$valor-despesa','depesa_ivestimento':{'$sum':'$valor-investimento-despesa'}}}}
     ]
     pipeline_impostos = [
-        {'$group':{'_id':{'codigo-projeto-unificado':'$codigo-projeto-unificado'},'despesa_impostos':{'$sum':'$valor-despesa'}}}
+        {'$group':{'_id':{'codigo-projeto-unificado':'$codigo-projeto-unificado'},'despesa_impostos':{'$sum':'$valor-despesa'},'depesa_ivestimento':{'$sum':'$valor-investimento-despesa'}}}
     ]
     pipeline_deducoes = [
-        {'$group':{'_id':{'codigo-projeto-unificado':'$codigo-projeto-unificado'},'despesa_deducoes':{'$sum':'$valor-despesa'}}}
+        {'$group':{'_id':{'codigo-projeto-unificado':'$codigo-projeto-unificado'},'despesa_deducoes':{'$sum':'$valor-despesa','depesa_ivestimento':{'$sum':'$valor-investimento-despesa'}}}}
     ]
 
     # Consultas que utilizam os filtros a cima. O retorno dessas consultas são dicionarios. Ex:. {'_id': {'descricao-projeto': 'ADM LOCAL (MACAÉ)'}, 'despesa_financeiro': 450681.34}
@@ -114,6 +114,8 @@ def total_despesa(db=back.db):
     for i in total_financeiro:
         if i['_id']['codigo-projeto-unificado'] not in dicio_geral:
             dicio_geral[i['_id']['codigo-projeto-unificado']] = i['despesa_financeiro']
+        elif i['_id']['codigo-projeto-unificado'] == 888:
+            dicio_geral[k['_id']['codigo-projeto-unificado']] += k['depesa_ivestimento']
         else:
             dicio_geral[i['_id']['codigo-projeto-unificado']] += i['despesa_financeiro']
 
@@ -122,6 +124,8 @@ def total_despesa(db=back.db):
         if j['_id']['codigo-projeto-unificado'] not in dicio_geral:
             # print(f'entrou no if com o valor de cc :{i['_id']['codigo-projeto-unificado']}')
             dicio_geral[j['_id']['codigo-projeto-unificado']] = j['despesa_folha']
+        elif j['_id']['codigo-projeto-unificado'] == 888:
+            dicio_geral[k['_id']['codigo-projeto-unificado']] += k['depesa_ivestimento']
         else:
             dicio_geral[j['_id']['codigo-projeto-unificado']] += j['despesa_folha']
     
@@ -130,18 +134,26 @@ def total_despesa(db=back.db):
     for k in total_relatorio:
         if k['_id']['codigo-projeto-unificado'] not in dicio_geral:
             dicio_geral[k['_id']['codigo-projeto-unificado']] = k['despesa_relatorio']
+        elif k['_id']['codigo-projeto-unificado'] == 888:
+            dicio_geral[k['_id']['codigo-projeto-unificado']] += k['depesa_ivestimento']
         else:    
             dicio_geral[k['_id']['codigo-projeto-unificado']] += k['despesa_relatorio']
+
+
 
     for x in total_impostos:
         if x['_id']['codigo-projeto-unificado'] not in dicio_geral:
             dicio_geral[x['_id']['codigo-projeto-unificado']] = x['despesa_impostos']
+        elif x['_id']['codigo-projeto-unificado'] == 888:
+            dicio_geral[k['_id']['codigo-projeto-unificado']] += k['depesa_ivestimento']
         else:    
             dicio_geral[x['_id']['codigo-projeto-unificado']] += x['despesa_impostos']
 
     for y in total_deducoes:
         if y['_id']['codigo-projeto-unificado'] not in dicio_geral:
             dicio_geral[y['_id']['codigo-projeto-unificado']] = y['despesa_deducoes']
+        elif y['_id']['codigo-projeto-unificado'] == 888:
+            dicio_geral[k['_id']['codigo-projeto-unificado']] += k['depesa_ivestimento']
         else:    
             dicio_geral[y['_id']['codigo-projeto-unificado']] += y['despesa_deducoes']
 
@@ -502,31 +514,31 @@ def cad_encargos(db=back.db):
 
     return lista_filial
 
-def detalha_despesas(competencia,contrato,db=back.db):
+def detalha_despesas(db=back.db):
     
     colecao_notas = db.get_collection('Despesas Relatório')
     colecao_fin = db.get_collection('Despesas Folha')
     colecao_folha = db.get_collection('Despesas Financeiro')
 
-    if competencia == None and contrato == None:
-        retorno_notas = colecao_notas.find()
-        retorno_fin = colecao_fin.find()
-        retorno_folha = colecao_folha.find()
+    # if competencia == None and contrato == None:
+    retorno_notas = colecao_notas.find()
+    retorno_fin = colecao_fin.find()
+    retorno_folha = colecao_folha.find()
     
-    elif competencia == None:
-        retorno_notas = colecao_notas.find({'descricao-projeto':contrato})
-        retorno_fin = colecao_fin.find({'descricao-projeto':contrato})
-        retorno_folha = colecao_folha.find({'descricao-projeto':contrato})
+    # elif competencia == None:
+    #     retorno_notas = colecao_notas.find({'descricao-projeto':contrato})
+    #     retorno_fin = colecao_fin.find({'descricao-projeto':contrato})
+    #     retorno_folha = colecao_folha.find({'descricao-projeto':contrato})
 
-    elif contrato == None:
-        retorno_notas = colecao_notas.find({'competencia-despesa':competencia})
-        retorno_fin = colecao_fin.find({'competencia-despesa':competencia})
-        retorno_folha = colecao_folha.find({'competencia-despesa':competencia})
+    # elif contrato == None:
+    #     retorno_notas = colecao_notas.find({'competencia-despesa':competencia})
+    #     retorno_fin = colecao_fin.find({'competencia-despesa':competencia})
+    #     retorno_folha = colecao_folha.find({'competencia-despesa':competencia})
     
-    else:
-        retorno_notas = colecao_notas.find({'descricao-projeto':contrato,'competencia-despesa':competencia})
-        retorno_fin = colecao_fin.find({'descricao-projeto':contrato,'competencia-despesa':competencia})
-        retorno_folha = colecao_folha.find({'descricao-projeto':contrato,'competencia-despesa':competencia})
+    # else:
+    #     retorno_notas = colecao_notas.find({'descricao-projeto':contrato,'competencia-despesa':competencia})
+    #     retorno_fin = colecao_fin.find({'descricao-projeto':contrato,'competencia-despesa':competencia})
+    #     retorno_folha = colecao_folha.find({'descricao-projeto':contrato,'competencia-despesa':competencia})
 
     # lista_notas = []
     # lista_fin = []
